fcdb5dc42deb1b516b8f04f9841a36ce8e67c696
Komitoray <foulard.aymeric@gmail.com>
2017-04-08 22:23:55 +0200

    create dorsal project

---
+++ assets/javascript/include_files.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+  $(function(){
+      $("#header").load("layout/shared/header.html");
+  });
+
+
+  $(function(){
+      $("#footer").load("layout/shared/footer.html");
+  });
+
+  $(function(){
+      $("#side-menu").load("layout/shared/side-menu.html");
+  });
+
+  $(function(){
+      $("#graph_1").load("layout/graphs/graph_1.html");
+  });
+});
